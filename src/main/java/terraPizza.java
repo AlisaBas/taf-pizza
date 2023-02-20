3b97c424fea6be48e3b936a1cd600d66b6f99f13
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-20 17:18:22 -0500

    terraPizza class added

---
+++ src/main/java/terraPizza.java
@@ -0,0 +1,9 @@
+public class terraPizza {
+    public static final String URL="https://terrapizza.by";
+    public static final String BTN_MENU="/html/body/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a";
+    public static final String CATALOG_PIZZA="/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[9]/a";
+    public static final String BTN_ADD_TO_BASKET="//*[@id=\"menu-card-364\"]/div[3]/div[3]/div[2]/div[2]/button";
+    public static final String BTN_TOP_BASKET="/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]";
+    public static final String BASKET_ITEM_NAME="//*[@id=\"basket-el-0\"]/div[2]/div[1]/div[1]";
+
+}
