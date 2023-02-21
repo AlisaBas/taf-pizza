5eb4cc1e4c28eaa3cf71761b7645e5dde39cc2a2
hanna-alisabasaraba <hannaalisa.basaraba@gmail.com>
2023-02-20 19:06:29 -0500

    refactor pizzaAndBeverageAdd test added

--- src/test/java/terraPizza.java
+++ src/test/java/terraPizza.java
@@ -5,5 +5,12 @@ public class terraPizza {
     public static final String BTN_ADD_TO_BASKET="//*[@id=\"menu-card-364\"]/div[3]/div[3]/div[2]/div[2]/button";
     public static final String BTN_TOP_BASKET="/html/body/div[2]/div[1]/div[3]/div[2]/div[1]/div[1]";
     public static final String BASKET_ITEM_NAME="//*[@id=\"basket-el-0\"]/div[2]/div[1]/div[1]";
+    public static final String CATALOG_BAR="/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[15]/a";
+    public static final String BTN_ADD_TO_BASKET1="//*[@id=\"menu-card-1732\"]/div[3]/div[3]/div/div[2]/button";
+    public static final String BASKET_ITEM_NAME1="//*[@id=\"basket-el-1\"]/div[2]/div[1]/div[1]";
+
+    public static final String BTN_BACK_TO_MENU ="/html/body/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a";
+
+
 
 }
--- src/test/java/terraPizzaTest.java
+++ src/test/java/terraPizzaTest.java
@@ -56,6 +56,61 @@ public class terraPizzaTest {
         driver.quit();
     }*/
 
+    @Test
+    public void testPizzaAndBeverageAdd(){
+        driver=new SafariDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
+        driver.get(terraPizza.URL);
+        WebElement buttonMenuElement= driver.findElement(By.xpath(terraPizza.BTN_MENU));
+        Util.waitThreadFor(7);
+        buttonMenuElement.click();
+        Util.waitThreadFor(5);
+        WebElement catalogPizzaElement= driver.findElement(By.xpath(terraPizza.CATALOG_PIZZA));
+        Util.waitThreadFor(5);
+        catalogPizzaElement.click();
+        Util.waitThreadFor(5);
+        WebElement buttonAddToBasketElement=driver.findElement(By.xpath(terraPizza.BTN_ADD_TO_BASKET));
+        Util.waitThreadFor(5);
+        buttonAddToBasketElement.click();
+        Util.waitThreadFor(5);
+        WebElement buttonBackTOMenuElement=driver.findElement(By.xpath(terraPizza.BTN_BACK_TO_MENU));
+        Util.waitThreadFor(3);
+        buttonBackTOMenuElement.click();
+        Util.waitThreadFor(5);
+        WebElement catalogBarElement=driver.findElement(By.xpath(terraPizza.CATALOG_BAR));
+        Util.waitThreadFor(3);
+        catalogBarElement.click();
+        Util.waitThreadFor(5);
+        WebElement buttonAddToBasket1Element=driver.findElement(By.xpath(terraPizza.BTN_ADD_TO_BASKET1));
+        Util.waitThreadFor(5);
+        buttonAddToBasket1Element.click();
+        WebElement topBasketElement=driver.findElement(By.xpath(terraPizza.BTN_TOP_BASKET));
+        Util.waitThreadFor(5);
+        topBasketElement.click();
+        Util.waitThreadFor(5);
+        WebElement basketItemNameElement=driver.findElement(By.xpath(terraPizza.BASKET_ITEM_NAME));
+        Util.waitThreadFor(3);
+        WebElement basketItemName1Element=driver.findElement(By.xpath(terraPizza.BASKET_ITEM_NAME1));
+        Util.waitThreadFor(5);
+        Assertions.assertEquals("\n" +
+                "                Пицца Маргарита                \n" +
+                "                                            Классическая\n" +
+                "                                                                32  см\n" +
+                "                                    \n" +
+                "            ",basketItemNameElement.getText());
+        Util.waitThreadFor(5);
+        Assertions.assertEquals("\n" +
+                "                Сок \"Rich\" яблоко (Россия)                \n" +
+                "                                                        \n" +
+                "            ", basketItemName1Element.getText());
+
+
+
+
+
+    }
+
 
 }
 
